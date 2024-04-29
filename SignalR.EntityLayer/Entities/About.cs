063216ccbe9cf9844ee40f6b8565b5084d8d45bc
aleynaflo <selekaleyna@gmail.com>
2024-04-30 00:17:11 +0300

    5-Entitylerin Tamamlanması

--- SignalR.EntityLayer/Entities/About.cs
+++ SignalR.EntityLayer/Entities/About.cs
@@ -1,9 +1,8 @@
-﻿namespace SignalRApi.DAL.Entities
+﻿namespace SignalR.EntityLayer.Entities
 {
-    public class Discount
+    public class About
     {
-        public int DiscountId { get; set; } 
-        public string Amount { get; set; } 
+        public int AboutId { get; set; } 
         public string Title { get; set; } 
         public string Description { get; set; } 
         public string ImageUrl { get; set; } 
